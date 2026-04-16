5fa774f7c52c14de282137295b786e56942704e0
Lucas Francelino <francelino25lucas@gmail.com>
2026-04-16 16:10:34 -0300

    revert category schema changes

--- src/schemas/firestore/category.ts
+++ src/schemas/firestore/category.ts
@@ -1,20 +1,12 @@
 import { z } from "zod";
 import { nonEmptyStringSchema } from "@/src/schemas/firestore/utils";
 
-type _Category = {
-  id: string;
-  parentId?: string;
-  name: string;
-  slug: string;
-  children: _Category[];
-};
 
-export const CategorySchema: z.ZodType<_Category> = z.object({
+export const CategorySchema = z.object({
   id: nonEmptyStringSchema,
   parentId: nonEmptyStringSchema.optional(),
   name: nonEmptyStringSchema,
   slug: nonEmptyStringSchema,
-  children: z.array(z.lazy(() => CategorySchema)).default([]),
 });
 
 export type Category = z.infer<typeof CategorySchema>;
